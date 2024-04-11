90ea827d0a339eae5963aac56b807f117875778f
Kelechi <ebirikele@gmail.com>
2024-04-11 23:57:56 +0100

    Size of tree 2

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,6 +1,20 @@
 #include "binary_tree.h"
 
 /**
+ * recursive_size - count size recursively
+ * @tree: pointer to root of tree
+ *
+ * Return: count
+ */
+size_t recursive_size(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (-1);
+
+	return (recursive_size(tree));
+}
+
+/**
  * binary_tree_size - Measure the size of binary tree
  * @tree: root node of tree
  *
@@ -8,11 +22,13 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	size_t size = 0;
+	size_t left = 0;
+	size_t right = 0;
 
 	if (!tree)
 		return (0);
-	size++;
-	binary_tree_preorder(tree->left);
-	binary_tree_preorder(tree->right);
+	left = recursive_size(tree->left) + 1;
+	right = recursive_size(tree->right) + 1;
+
+	return (left + right + 1);
 }
