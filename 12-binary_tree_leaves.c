513a2cead118eb3a318e658851c88db39c7c2b60
Kelechi <ebirikele@gmail.com>
2024-04-12 00:32:34 +0100

    Count tree leaves

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -12,6 +12,6 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	return (binary_tree_leaves(tree->left) &&
-			binary_tree_leaves(tree->right) + 1);
+	return (binary_tree_leaves(tree->left) == NULL &&
+			binary_tree_leaves(tree->right) == NULL);
 }
