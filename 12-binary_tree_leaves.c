8454b86fccdd6c130613b455ff82aa91846331ec
Kelechi <ebirikele@gmail.com>
2024-04-12 00:50:40 +0100

    Count tree leaves 1

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,5 +1,20 @@
 #include "binary_trees_h"
 
+/**
+ * is_leaf - check if node is leaf
+ * @tree: pointer to node to check
+ *
+ * Return: 1 if leaf, 0 if not
+ */
+
+int is_leaf(const binary_tree_t *tree)
+{
+	int leaf = 0;
+
+	if (tree && !(tree->left) && !(tree->right))
+		leaf = 1;
+	return (leaf);
+}
 
 /**
  * binary_tree_leaves - Count leaves
@@ -12,6 +27,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	return (binary_tree_leaves(tree->left) == NULL &&
-			binary_tree_leaves(tree->right) == NULL);
+	if (is_leaf(tree))
+		return (1);
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
