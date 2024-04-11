de821a62618edf459f7c639675b3a192151f5bc3
Kelechi <ebirikele@gmail.com>
2024-04-11 23:01:11 +0100

    Depth of tree

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -15,12 +15,8 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 		return (0);
 
 	depth = 0;
-
-	if (tree->parent != NULL)
-	{
-		binary_tree_depth(tree->parent);
-		depth++;
-	}
+	binary_tree_depth(tree->parent);
+	depth++;
 
 	return (depth);
 }
