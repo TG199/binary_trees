4f9ba1be1334c6843fd06911163e00f1a6a3d6bb
Kelechi <ebirikele@gmail.com>
2024-04-12 03:00:54 +0100

    Sorted AVL

--- binary_trees.h
+++ binary_trees.h
@@ -72,6 +72,7 @@ avl_t *avl_insert(avl_t **tree, int value);
 avl_t *array_to_avl(int *array, size_t size);
 avl_t *avl_remove(avl_t *root, int value);
 avl_t *sorted_array_to_avl(int *array, size_t size);
+void create_tree(avl_t **node, int *array, size_t size, int mode);
 int binary_tree_is_heap(const binary_tree_t *tree);
 heap_t *heap_insert(heap_t **root, int value);
 heap_t *array_to_heap(int *array, size_t size);
